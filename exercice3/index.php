4c68a208dd4c0b422755e9992d2386cdedd243c1
Pierre <a@a.a>
2020-12-09 16:29:48 +0100

    maj exo 3

--- exercice3/index.php
+++ exercice3/index.php
@@ -1,7 +1,7 @@
 <?php
-    $admin = 'pierre';
-    $color = 'red';
-    $font = 'bold';
+
+// création de la variable
+    $admin = true;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,23 +11,55 @@
     <title>exo 3</title>
     <style>
         h1{
-            color : <?php echo $color;?>;
-            font-weight: <?php echo $font;?>;
+            color : 'red';
+            font-weight: 'bold';
         }
     </style>
 </head>
 <body>
 
+
+
+
+
+
+
 <?php
 
-if($admin == 'pierre'){
-echo 'Bienvenu Pierre, voici le lien pour ton <a href="https://www.artstation.com/creagameart">portfolio</a> ';
+// Même si la balise PHP se ferme avant la fermeture du if, le coe HTML déclaré entre sera considéré comme faisant partie de la condition
+if($admin) {
+
+    ?>
+    echo '<p>Bienvenu Pierre, voici le lien pour ton <a href="https://www.artstation.com/creagameart">portfolio</a></p> ';
+    <?php
+
+} else {
+
+    ?>
+    <h1>Nope, tu est un intrus, tu mérite le buché !</h1>';
+    <?php
+}
+
+?>
+
+
+
+
+
+
+
+
+<?php
+
+if($admin/*si la variable est un booléan, juste mettre la variable sans = true, car la déclaration est déjà en true*/){
+echo '<p>Bienvenu Pierre, voici le lien pour ton <a href="https://www.artstation.com/creagameart">portfolio</a></p> ';
 }
 else {
     echo '<h1>Nope, tu est un intrus, tu mérite le buché !</h1>';
 }
 
 ?>
+
 
 </body>
 </html>
