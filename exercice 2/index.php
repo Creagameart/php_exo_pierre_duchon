bf196f25819af6781bf8626d671a5bfac49d9f12
Pierre <a@a.a>
2020-12-09 14:13:45 +0100

    maj exo 2

--- exercice 2/index.php
+++ exercice 2/index.php
@@ -5,14 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>exercice 2</title>
 </head>
-<body>
+<?php
+    echo "<body bgcolor='purple'>";
+    ?>
+
 
     <?php
     $name = 'henry';
 
     echo '<h1>Je me présente, je m\'appelle '.$name.'!</h1>';
-    echo "<body bgcolor='purple'>";
+    
     ?>
+
+
 
 </body>
 </html>
