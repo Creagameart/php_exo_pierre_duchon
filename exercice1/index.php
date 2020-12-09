2cd8a787df38bd2797b259f9a08268dced855170
Pierre <a@a.a>
2020-12-09 11:46:30 +0100

    fin de l'exercice 1

---
+++ exercice1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo 1</title>
+</head>
+<body>
+    <h1>liste de fruits</h1>
+
+    <?php
+    
+    echo'<ul> fruits
+            <li>papaye</li>
+            <li>fruit du dragon</li>
+            <li>durian</li>
+            <li>concombre</li>
+            <li>pomme</li>
+        </ul>'
+
+    ?>
+</body>
+</html>
