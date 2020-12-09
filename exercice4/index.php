90f523ece1993e70c6c69bd25f6a2a5f1c282084
Pierre <a@a.a>
2020-12-09 21:15:04 +0100

    fin exercice4

---
+++ exercice4/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo 4</title>
+</head>
+<body>
+
+<?php
+    $i = 0 + 1;
+
+    while ($i-1 < 5000){
+    echo '<ul><li>'.$i.'</li></ul>';
+    $i++;
+    }
+    
+?>
+
+</body>
+</html>
