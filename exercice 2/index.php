8d3115bea9399bce3e74365ca5009067c9f4931d
Pierre <a@a.a>
2020-12-09 13:39:08 +0100

    fin de l'exercice 2

---
+++ exercice 2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice 2</title>
+</head>
+<body>
+
+    <?php
+    $name = 'henry';
+
+    echo '<h1>Je me présente, je m\'appelle '.$name.'!</h1>';
+    echo "<body bgcolor='purple'>";
+    ?>
+
+</body>
+</html>
